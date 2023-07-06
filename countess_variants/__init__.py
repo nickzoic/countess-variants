e68d6e577834041eee918b05fc42a3f7361b8997
Nick Moore <nick@zoic.org>
2023-07-06 16:17:59 +1000

    ... still fixing

--- countess_variants/__init__.py
+++ countess_variants/__init__.py
@@ -3,31 +3,17 @@
 import re
 from typing import Mapping, Optional
 
-from more_itertools import chunked
-
 import pandas as pd
-
 from countess.core.logger import Logger
-from countess.core.parameters import (
-    BooleanParam,
-    ChoiceParam,
-    ColumnChoiceParam,
-    IntegerParam,
-    StringParam,
-    StringCharacterSetParam,
-    FileParam,
-)
+from countess.core.parameters import (BooleanParam, ColumnChoiceParam,
+                                      StringCharacterSetParam)
 from countess.core.plugins import PandasTransformPlugin
+from more_itertools import chunked
 
 from .caller import find_variant_string
 
-VERSION = '0.0.1'
+VERSION = "0.0.1"
 
-MM2_PRESET_CHOICES = ["sr", "map-pb", "map-ont", "asm5", "asm10", "splice"]
-
-
-def triplets(seq):
-    return [ tuple(x) for x in chunked(seq, 3) ]
 
 class VariantsPlugin(PandasTransformPlugin):
     """Turns a DNA sequence into a HGVS variant code"""
@@ -38,7 +24,7 @@ class VariantsPlugin(PandasTransformPlugin):
     version = VERSION
     link = "https://github.com/CountESS-Project/countess-variants#readme"
 
-    CHARACTER_SET = set(['A', 'C', 'G', 'T'])
+    CHARACTER_SET = set(["A", "C", "G", "T"])
 
     parameters = {
         "column": ColumnChoiceParam("Input Column", "sequence"),
@@ -48,18 +34,15 @@ class VariantsPlugin(PandasTransformPlugin):
     }
 
     def process(self, value):
+        """process a single sequence into a variant string"""
         return find_variant_string(
-            self.parameters["ref"].value,
-            value,
-            triplet_mode = self.parameters["triplet"].value
+            self.parameters["ref"].value, value, triplet_mode=self.parameters["triplet"].value
         )
-        
+
     def run_df(self, df: pd.DataFrame, logger: Logger) -> pd.DataFrame:
+        """process dataframe"""
+        assert isinstance(self.parameters["column"], ColumnChoiceParam)
 
-        assert isinstance(self.parameters['column'], ColumnChoiceParam)
-
-        column = self.parameters['column'].get_column(df)
+        column = self.parameters["column"].get_column(df)
 
         return df.assign(variant=column.apply(self.process))
-
-
